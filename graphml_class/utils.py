7a3ad6cece98debf5c466e7b8275db57a20bdd4d
Russell Jurney <russell.jurney@gmail.com>
2023-09-21 17:02:56 -0700

    Some utils for encoding papers using sentence encoding / sentence transformers such as paraphrase-MiniLM-L6-v2

---
+++ graphml_class/utils.py
@@ -0,0 +1,141 @@
+import gzip
+import io
+import re
+import tarfile
+import typing
+from typing import Dict
+
+import networkx as nx
+import numpy as np
+import requests
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L6-v2")
+
+
+def extract_paper_info(record):
+    """Extract structured information from the text of academic paper text records using regular expressions.
+
+    Note: I was written wholly or in part by ChatGPT4 on May 23, 2023.
+    """
+
+    # Initialize an empty dictionary to hold the information
+    info = {}
+
+    # Match "Paper" field
+    paper_match = re.search(r"Paper:\s*(.*)", record)
+    if paper_match:
+        info["Paper"] = paper_match.group(1)
+
+    # # Match "From" field
+    # from_match = re.search(r"From:\s*(.*)", record)
+    # if from_match:
+    #     info['From'] = from_match.group(1)
+
+    # Match "From" field
+    from_match = re.search(r"From:\s*([^<]*)<", record)
+    if from_match:
+        info["From"] = from_match.group(1).strip()
+
+    # Match "Date" field
+    date_match = re.search(r"Date:\s*(.*)", record)
+    if date_match:
+        info["Date"] = date_match.group(1)
+
+    # Match "Title" field
+    title_match = re.search(r"Title:\s*(.*)", record)
+    if title_match:
+        info["Title"] = title_match.group(1)
+
+    # Match "Authors" field
+    authors_match = re.search(r"Authors:\s*(.*)", record)
+    if authors_match:
+        info["Authors"] = authors_match.group(1)
+
+    # Match "Comments" field
+    comments_match = re.search(r"Comments:\s*(.*)", record)
+    if comments_match:
+        info["Comments"] = comments_match.group(1)
+
+    # Match "Report-no" field
+    report_no_match = re.search(r"Report-no:\s*(.*)", record)
+    if report_no_match:
+        info["Report-no"] = report_no_match.group(1)
+
+    # Match "Journal-ref" field
+    journal_ref_match = re.search(r"Journal-ref:\s*(.*)", record)
+    if journal_ref_match:
+        info["Journal-ref"] = journal_ref_match.group(1)
+
+    # Extract "Abstract" field
+    abstract_pattern = r"Journal-ref:[^\\\\]*\\\\[\n\s]*(.*?)(?=\\\\)"
+    abstract_match = re.search(abstract_pattern, record, re.DOTALL)
+    if abstract_match:
+        abstract = abstract_match.group(1)
+        abstract = abstract.replace("\n", " ").replace("  ", " ")
+        info["Abstract"] = abstract.strip()
+
+    return info
+
+
+def embed_paper_info(records: typing.Union[str, typing.List[str]]) -> np.ndarray:
+    if records and isinstance(records, str):
+        records = [records]
+    return model.encode(records)
+
+
+def compare_paper_embeddings(X, Y):
+    return cosine_similarity(X, Y)
+
+
+def main():
+    """main _summary_"""
+    # Initialize a directed graph
+    G = nx.DiGraph()
+
+    # Download and load edges (citations) from `cit-HepTh.txt.gz`
+    response = requests.get("https://snap.stanford.edu/data/cit-HepTh.txt.gz")
+    gzip_content = io.BytesIO(response.content)
+
+    with open("data/cit-HepTh.txt.gz", "wb") as f:
+        f.write(response.content)
+        print("Wrote downloaded edge file to data/cit-HepTh.txt.gz")
+
+    # We need to create sequential IDs starting from 0 for littleballoffur
+    file_to_fur: Dict[int, int] = {}
+    fur_to_file: Dict[int, int] = {}
+    current_idx = 0
+
+    # Decompress the gzip content and build the edge list for our network
+    with gzip.GzipFile(fileobj=gzip_content) as f:
+        for line_number, line in enumerate(f):
+            line = line.decode("utf-8")
+
+            # Ignore comment lines that start with '#'
+            if not line.startswith("#"):
+                # Source (citing), desstination (cited) papers
+                citing_key, cited_key = line.strip().split("\t")
+                citing_key, cited_key = int(citing_key), int(cited_key)
+
+                # If the either of the paper IDs don't exist, make one
+                for key in [citing_key, cited_key]:
+                    if key not in file_to_fur:
+                        # Build up an index that maps back and forth
+                        file_to_fur[key] = current_idx
+                        fur_to_file[current_idx] = key
+
+                        # Bump the current ID
+                        current_idx += 1
+
+                # print(f"Citing key: {citing_key}, Cited key: {cited_key}")
+                # print(f"Mapped key: {file_to_fur[citing_key]}, Mapped key: {file_to_fur[cited_key]}")
+
+                G.add_edge(file_to_fur[citing_key], file_to_fur[cited_key])
+
+                # Conditionally set the keys on the nodes
+                G.nodes[file_to_fur[citing_key]]["file_id"] = citing_key
+                G.nodes[file_to_fur[citing_key]]["sequential_id"] = file_to_fur[citing_key]
+
+                G.nodes[file_to_fur[cited_key]]["file_id"] = cited_key
+                G.nodes[file_to_fur[cited_key]]["sequential_id"] = file_to_fur[cited_key]
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,92 @@
+import timeit
+import typing
+
+import numpy as np
+from graphml_class.utils import embed_paper_info, extract_paper_info
 from pytest import fixture
-from graphml_class.utils import extract_paper_info
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-def test_extract_paper_info(get_docs)
+@fixture
+def get_docs():
+    docs = [
+        """------------------------------------------------------------------------------
+\\
+Paper: hep-th/9612115
+From: Asato Tsuchiya <tsuchiya@theory.kek.jp>
+Date: Wed, 11 Dec 1996 17:38:56 +0900   (20kb)
+Date (revised): Tue, 31 Dec 1996 01:06:34 +0900
+
+Title: A Large-N Reduced Model as Superstring
+Authors: N. Ishibashi, H. Kawai, Y. Kitazawa and A. Tsuchiya
+Comments: 29 pages, Latex, a footnote and references added, eq.(3.52)
+  corrected, minor corrections
+Report-no: KEK-TH-503, TIT/HEP-357
+Journal-ref: Nucl.Phys. B498 (1997) 467-491
+\\
+  A matrix model which has the manifest ten-dimensional N=2 super Poincare
+invariance is proposed. Interactions between BPS-saturated states are analyzed
+to show that massless spectrum is the same as that of type IIB string theory.
+It is conjectured that the large-N reduced model of ten-dimensional super
+Yang-Mills theory can be regarded as a constructive definition of this model
+and therefore is equivalent to superstring theory.
+\\
+""",
+        """------------------------------------------------------------------------------
+\\
+Paper: hep-th/9711029
+From: John Schwarz <jhs@theory.caltech.edu>
+Date: Wed, 5 Nov 1997 17:30:55 GMT   (20kb)
+Date (revised v2): Thu, 6 Nov 1997 23:52:45 GMT   (21kb)
+
+Title: The Status of String Theory
+Author: John H. Schwarz
+Comments: 16 pages, latex, two figures; minor corrections, references added
+Report-no: CALT-68-2140
+\\
+  There have been many remarkable developments in our understanding of
+superstring theory in the past few years, a period that has been described as
+``the second superstring revolution.'' Several of them are discussed here. The
+presentation is intended primarily for the benefit of nonexperts.
+\\
+""",
+    ]
+    return docs
+
+
+def test_extract_paper_info(get_docs: typing.List[str]) -> None:
+    """test_extract_paper_info Test extracting fields from paper info
+
+    Parameters
+    ----------
+    get_docs : typing.List[str]
+        fixture containing two paper documents
+    """
+    (doc1, doc2) = get_docs
+
+    paper_info = extract_paper_info(doc1)
+    # Get the paper ID part of the "Paper" field
+    paper_id = int(paper_info.get("Paper", "").split("/")[-1])
+    assert paper_info["Paper"] == "hep-th/9612115"
+    assert paper_id == 9612115
+
+    paper_info = extract_paper_info(doc2)
+    # Get the paper ID part of the "Paper" field
+    paper_id = int(paper_info.get("Paper", "").split("/")[-1])
+    assert paper_info["Paper"] == "hep-th/9711029"
+    assert paper_id == 9711029
+
+
+def test_embed_paper_info(get_docs: typing.List[str]) -> None:
+    """test_embed_paper_info Test embedding our paper documents into vector embeddings.
+
+    Parameters
+    ----------
+    get_docs : typing.List[str]
+        fixture containing two paper documents
+    """
+    emb_docs = embed_paper_info(get_docs)
+    assert emb_docs.shape == (2, 384)
+    assert (
+        cosine_similarity(emb_docs[0].reshape(1, -1), emb_docs[1].reshape(1, -1))[0, 0] - 0.7496432
+    ) < 0.0001

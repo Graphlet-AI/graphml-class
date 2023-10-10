855c39d9db615f1c4ca2d4c3b3ddc5dd2e1380b1
Russell Jurney <russell.jurney@gmail.com>
2023-10-10 02:44:18 -0700

    Categorical palette to import into notebooks

---
+++ graphml_class/palette.py
@@ -0,0 +1,36 @@
+# A big palette from 3 different palettes from: https://www.heavy.ai/blog/12-color-palettes-for-telling-better-stories-with-your-data
+CATEGORICAL_PALETTE: List[str] = (
+    [
+        "#ea5545",
+        "#f46a9b",
+        "#ef9b20",
+        "#edbf33",
+        "#ede15b",
+        "#bdcf32",
+        "#87bc45",
+        "#27aeef",
+        "#b33dc6",
+    ]
+    + [
+        "#b30000",
+        "#7c1158",
+        "#4421af",
+        "#1a53ff",
+        "#0d88e6",
+        "#00b7c7",
+        "#5ad45a",
+        "#8be04e",
+        "#ebdc78",
+    ]
+    + [
+        "#fd7f6f",
+        "#7eb0d5",
+        "#b2e061",
+        "#bd7ebe",
+        "#ffb55a",
+        "#ffee65",
+        "#beb9db",
+        "#fdcce5",
+        "#8bd3c7",
+    ]
+)

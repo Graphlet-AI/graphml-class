54364d8ab3756eb5ae79a5b01614105d11e0e35d
Russell Jurney <russell.jurney@gmail.com>
2023-10-23 08:49:26 -0700

    Actually runs end to end

--- graphml_class/motif.py
+++ graphml_class/motif.py
@@ -1,4 +1,5 @@
 import os
+import resource
 from uuid import uuid4
 
 import pandas as pd
@@ -18,6 +19,19 @@ os.environ[
 pd.set_option("display.max_columns", None)
 
 
+def get_memory_usage():
+    """Return the resident memory usage in GB."""
+    return (
+        resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        + resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
+    ) / (1024 * 1024)
+
+
+def print_memory_usage():
+    """Print the resident memory usage in GB."""
+    print(f"Memory usage: {get_memory_usage():.2f} GB")
+
+
 #
 # Initialize a SparkSession. You can configre SparkSession via: .config("spark.some.config.option", "some-value")
 #
@@ -25,8 +39,8 @@ spark = (
     SparkSession.builder.appName("Stack Overflow Motif Analysis")
     # Lets the Id:(Stack Overflow int) and id:(GraphFrames ULID) coexist
     .config("spark.sql.caseSensitive", True)
-    .config("spark.driver.memory", "96g")
-    .getOrCreate()
+    # Single node mode - 128GB machine
+    .config("spark.driver.memory", "96g").getOrCreate()
 )
 
 
@@ -101,3 +115,103 @@ nodes_df = spark.read.parquet(NODES_PATH)
 
 # Helps performance of GraphFrames' algorithms
 nodes_df = nodes_df.cache()
+
+
+#
+# Build the edges DataFrame: VotedFor, AskedBy, AnsweredBy.
+# Remember: 'src', 'dst' and 'relationship' are standard edge fields in GraphFrames
+# Remember: we must produce src/dst based on lowercase 'id' UUID, not 'Id' which is Stack Overflow's integer.
+#
+
+# Make sure we have the right columns and cached data
+votes_df = nodes_df.filter(nodes_df.Type == "Vote")
+posts_df = nodes_df.filter(nodes_df.Type == "Post")
+users_df = nodes_df.filter(nodes_df.Type == "User")
+
+
+#
+# Building blocks: separate the questions and answers
+#
+
+# Do the questions look ok? Questions have NO parent ID and DO have a Title
+questions_df = posts_df[posts_df.ParentId.isNull()]
+print(f"\nTotal questions: {questions_df.count():,}\n")
+
+questions_df.select("ParentId", "Title", "Body").show(10)
+
+# Answers DO have a ParentId parent post and no Title
+answers_df = posts_df[posts_df.ParentId.isNotNull()]
+print(f"\nTotal answers: {answers_df.count():,}\n")
+
+answers_df.select("ParentId", "Title", "Body").show(10)
+
+
+#
+# Create a [Vote]--VotedFor-->[Post] edge
+#
+src_vote_df = votes_df.select(
+    F.col("id").alias("src"),
+    F.col("Id").alias("VoteId"),
+    # Everything has all the fields - should build from base records but need UUIDs
+    F.col("PostId").alias("VotePostId"),
+)
+voted_for_edge_df = (
+    src_vote_df.join(posts_df, src_vote_df.VotePostId == posts_df.Id)
+    .select(
+        # 'src' comes from the votes' 'id'
+        "src",
+        # 'dst' comes from the posts' 'id'
+        F.col("id").alias("dst"),
+        # All edges have a 'relationship' field
+        F.lit("VotedFor").alias("relationship"),
+    )
+    .cache()
+)
+print(f"Total edges: {voted_for_edge_df.count():,}")
+
+
+#
+# Create a answer[Post]--Answers-->question[Post] edge
+#
+src_answers_df = answers_df.select(
+    F.col("id").alias("src"),
+    F.col("Id").alias("AnswerId"),
+    F.col("ParentId").alias("AnswerParentId"),
+)
+answered_edges_df = (
+    src_answers_df.join(posts_df, src_answers_df.AnswerParentId == posts_df.Id)
+    .select(
+        # 'src' comes from the answers' 'id'
+        "src",
+        # 'dst' comes from the posts' 'id'
+        F.col("id").alias("dst"),
+        # All edges have a 'relationship' field
+        F.lit("Answers").alias("relationship"),
+    )
+    .cache()
+)
+print(f"\nTotal Answered edges: {answered_edges_df.count():,}\n")
+
+
+#
+# Create a [User]--Posted-->[Post] edge
+#
+src_posts_df = posts_df.select(
+    F.col("id").alias("src"),
+    F.col("Id").alias("PostPostId"),
+    F.col("OwnerUserId").alias("PostOwnerUserId"),
+)
+
+posted_edge_df = (
+    src_posts_df.join(users_df, src_posts_df.PostOwnerUserId == users_df.Id)
+    .select(
+        # 'src' comes from the posts' 'id'
+        "src",
+        # 'dst' comes from the users' 'id'
+        F.col("id").alias("dst"),
+        # All edges have a 'relationship' field
+        F.lit("Posted").alias("relationship"),
+    )
+    .cache()
+)
+print(f"Total edges: {posted_edge_df.count():,}")

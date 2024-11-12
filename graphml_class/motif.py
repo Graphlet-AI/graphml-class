5b099be9610e2a250404e1800cc4c0eb4a436c4e
Russell Jurney <russell.jurney@gmail.com>
2024-11-11 20:25:23 -0800

    Type hints throughout, added User-Answered->Question relationship, gave all noded a common column sort order, about to instantiate the GraphFrame

--- graphml_class/motif.py
+++ graphml_class/motif.py
@@ -1,19 +1,19 @@
 import os
 import resource
-from uuid import uuid4
+from typing import List
 
 import pandas as pd
 import pyspark.sql.functions as F
 from graphframes import GraphFrame
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
 # This is actually already set in Docker, just reminding you Java is needed
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
 
 # Setup PySpark to use the GraphFrames jar package from maven central
-os.environ[
-    "PYSPARK_SUBMIT_ARGS"
-] = "--packages graphframes:graphframes:0.8.3-spark3.5-s_2.12 pyspark-shell"
+os.environ["PYSPARK_SUBMIT_ARGS"] = (
+    "--packages graphframes:graphframes:0.8.3-spark3.5-s_2.12 pyspark-shell"
+)
 
 # Show all the rows in a pd.DataFrame
 pd.set_option("display.max_columns", None)
@@ -35,7 +35,7 @@ def print_memory_usage():
 #
 # Initialize a SparkSession. You can configre SparkSession via: .config("spark.some.config.option", "some-value")
 #
-spark = (
+spark: SparkSession = (
     SparkSession.builder.appName("Stack Overflow Motif Analysis")
     # Lets the Id:(Stack Overflow int) and id:(GraphFrames ULID) coexist
     .config("spark.sql.caseSensitive", True)
@@ -47,7 +47,7 @@ spark = (
 #
 # Load the Posts...
 #
-posts_df = spark.read.parquet("data/stats.meta.stackexchange.com/Posts.parquet").withColumn(
+posts_df: DataFrame = spark.read.parquet("data/stats.meta.stackexchange.com/Posts.parquet").withColumn(
     "Type", F.lit("Post")
 )
 
@@ -57,7 +57,7 @@ print(f"\nTotal stats.meta.stackexchange.com Posts: {posts_df.count():,}\n")
 #
 # Load the Users...
 #
-users_df = spark.read.parquet("data/stats.meta.stackexchange.com/Users.parquet").withColumn(
+users_df: DataFrame = spark.read.parquet("data/stats.meta.stackexchange.com/Users.parquet").withColumn(
     "Type", F.lit("User")
 )
 print(f"\nTotal Users: {users_df.count():,}\n")
@@ -66,7 +66,7 @@ print(f"\nTotal Users: {users_df.count():,}\n")
 #
 # Load the Votes...
 #
-votes_df = spark.read.parquet("data/stats.meta.stackexchange.com/Votes.parquet").withColumn(
+votes_df: DataFrame = spark.read.parquet("data/stats.meta.stackexchange.com/Votes.parquet").withColumn(
     "Type", F.lit("Vote")
 )
 print(f"Total Votes: {votes_df.count():,}")
@@ -75,46 +75,51 @@ print(f"Total Votes: {votes_df.count():,}")
 #
 # Form the nodes from the UNION of posts, users, votes and their combined schemas
 #
-all_cols = set(
-    list(zip(posts_df.columns, posts_df.schema))
-    + list(zip(users_df.columns, users_df.schema))
-    + list(zip(votes_df.columns, votes_df.schema))
+all_cols: List[str] = list(
+    set(
+        list(zip(posts_df.columns, posts_df.schema))
+        + list(zip(users_df.columns, users_df.schema))
+        + list(zip(votes_df.columns, votes_df.schema))
+    )
 )
+all_column_names: List[str] = sorted([x[0] for x in all_cols])
 
 
 def add_missing_columns(df, all_cols):
     """Add any missing columns from any DataFrame among several we want to merge."""
     for col_name, schema_field in all_cols:
         if col_name not in df.columns:
-            df = df.withColumn(col_name, F.lit(None).cast(schema_field.dataType))
+            df: DataFrame = df.withColumn(col_name, F.lit(None).cast(schema_field.dataType))
     return df
 
 
 # Now apply this function to each of your DataFrames to get a consistent schema
-posts_df = add_missing_columns(posts_df, all_cols)
-users_df = add_missing_columns(users_df, all_cols)
-votes_df = add_missing_columns(votes_df, all_cols)
-assert set(posts_df.columns) == set(users_df.columns) == set(votes_df.columns)
+posts_df: DataFrame = add_missing_columns(posts_df, all_cols).select(all_column_names)
+users_df: DataFrame = add_missing_columns(users_df, all_cols).select(all_column_names)
+votes_df: DataFrame = add_missing_columns(votes_df, all_cols).select(all_column_names)
+assert (
+    set(posts_df.columns) == set(users_df.columns) == set(votes_df.columns) == set(all_column_names)
+)
 
 # Now union them together and remove duplicates
-nodes_df = posts_df.unionByName(users_df).unionByName(votes_df).distinct()
+nodes_df: DataFrame = posts_df.unionByName(users_df).unionByName(votes_df).distinct()
 print(f"Total distinct nodes: {nodes_df.count():,}")
 
 # Now add a unique ID field
-nodes_df = nodes_df.withColumn("id", F.expr("uuid()"))
+nodes_df: DataFrame = nodes_df.withColumn("id", F.expr("uuid()")).select("id", *all_column_names)
 
 
 #
 # Store the nodes to disk, reload and cache
 #
-NODES_PATH = "data/stats.meta.stackexchange.com/Nodes.parquet"
+NODES_PATH: str = "data/stats.meta.stackexchange.com/Nodes.parquet"
 
 # Write to disk and back again
 nodes_df.write.mode("overwrite").parquet(NODES_PATH)
-nodes_df = spark.read.parquet(NODES_PATH)
+nodes_df: DataFrame = spark.read.parquet(NODES_PATH)
 
 # Helps performance of GraphFrames' algorithms
-nodes_df = nodes_df.cache()
+nodes_df: DataFrame = nodes_df.cache()
 
 
 #
@@ -124,9 +129,9 @@ nodes_df = nodes_df.cache()
 #
 
 # Make sure we have the right columns and cached data
-votes_df = nodes_df.filter(nodes_df.Type == "Vote")
-posts_df = nodes_df.filter(nodes_df.Type == "Post")
-users_df = nodes_df.filter(nodes_df.Type == "User")
+votes_df: DataFrame = nodes_df.filter(nodes_df.Type == "Vote")
+posts_df: DataFrame = nodes_df.filter(nodes_df.Type == "Post")
+users_df: DataFrame = nodes_df.filter(nodes_df.Type == "User")
 
 
 #
@@ -134,13 +139,13 @@ users_df = nodes_df.filter(nodes_df.Type == "User")
 #
 
 # Do the questions look ok? Questions have NO parent ID and DO have a Title
-questions_df = posts_df[posts_df.ParentId.isNull()]
+questions_df: DataFrame = posts_df[posts_df.ParentId.isNull()]
 print(f"\nTotal questions: {questions_df.count():,}\n")
 
 questions_df.select("ParentId", "Title", "Body").show(10)
 
 # Answers DO have a ParentId parent post and no Title
-answers_df = posts_df[posts_df.ParentId.isNotNull()]
+answers_df: DataFrame = posts_df[posts_df.ParentId.isNotNull()]
 print(f"\nTotal answers: {answers_df.count():,}\n")
 
 answers_df.select("ParentId", "Title", "Body").show(10)
@@ -149,13 +154,13 @@ answers_df.select("ParentId", "Title", "Body").show(10)
 #
 # Create a [Vote]--VotedFor-->[Post] edge
 #
-src_vote_df = votes_df.select(
+src_vote_df: DataFrame = votes_df.select(
     F.col("id").alias("src"),
     F.col("Id").alias("VoteId"),
     # Everything has all the fields - should build from base records but need UUIDs
     F.col("PostId").alias("VotePostId"),
 )
-voted_for_edge_df = (
+voted_for_edge_df: DataFrame = (
     src_vote_df.join(posts_df, src_vote_df.VotePostId == posts_df.Id)
     .select(
         # 'src' comes from the votes' 'id'
@@ -167,18 +172,31 @@ voted_for_edge_df = (
     )
     .cache()
 )
-print(f"Total edges: {voted_for_edge_df.count():,}")
+print(f"Total VotedFor edges: {voted_for_edge_df.count():,}")
+print(f"Percentage of linked votes: {voted_for_edge_df.count() / votes_df.count():.2%}\n")
 
+#
+# Create a answer[User]--Answered-->question[Post] edge
+#
+asked_edges_df: DataFrame = questions_df.select(
+    F.col("OwnerUserId").alias("src"),
+    F.col("id").alias("dst"),
+    F.lit("Asked").alias("relationship"),
+)
+print(f"\nTotal Asked edges: {asked_edges_df.count():,}\n")
+print(
+    f"Percentage of asked questions linked to users: {asked_edges_df.count() / questions_df.count():.2%}\n"
+)
 
 #
 # Create a answer[Post]--Answers-->question[Post] edge
 #
-src_answers_df = answers_df.select(
+src_answers_df: DataFrame = answers_df.select(
     F.col("id").alias("src"),
     F.col("Id").alias("AnswerId"),
     F.col("ParentId").alias("AnswerParentId"),
 )
-answered_edges_df = (
+answered_edges_df: DataFrame = (
     src_answers_df.join(posts_df, src_answers_df.AnswerParentId == posts_df.Id)
     .select(
         # 'src' comes from the answers' 'id'
@@ -191,18 +209,19 @@ answered_edges_df = (
     .cache()
 )
 print(f"\nTotal Answered edges: {answered_edges_df.count():,}\n")
+print(f"Percentage of linked answers: {answered_edges_df.count() / answers_df.count():.2%}\n")
 
 
 #
 # Create a [User]--Posted-->[Post] edge
 #
-src_posts_df = posts_df.select(
+src_posts_df: DataFrame = posts_df.select(
     F.col("id").alias("src"),
     F.col("Id").alias("PostPostId"),
     F.col("OwnerUserId").alias("PostOwnerUserId"),
 )
 
-posted_edge_df = (
+posted_edge_df: DataFrame = (
     src_posts_df.join(users_df, src_posts_df.PostOwnerUserId == users_df.Id)
     .select(
         # 'src' comes from the posts' 'id'
@@ -214,4 +233,10 @@ posted_edge_df = (
     )
     .cache()
 )
-print(f"Total edges: {posted_edge_df.count():,}")
+print(f"Total Posted edges: {posted_edge_df.count():,}")
+print(f"Percentage of linked posts: {posted_edge_df.count() / posts_df.count():.2%}\n")
+
+#
+# Combine all the edges together into one relationships DataFrame
+#
+relationships: DataFrame = 

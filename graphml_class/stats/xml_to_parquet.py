fff6cdb9558a4b4aa6ea2f8a42d9bbee58f76dc4
Russell Jurney <russell.jurney@gmail.com>
2024-11-15 20:12:24 -0800

    Now converting everything including PostLinks, PostHistory, Tags and Badges. Now loading XML package in Python code

--- graphml_class/stats/xml_to_parquet.py
+++ graphml_class/stats/xml_to_parquet.py
@@ -1,10 +1,35 @@
 # Convert some Stack Exchange XML files to Parquet format
 #
-# Usage: pyspark --packages com.databricks:spark-xml_2.13:0.17.0
+# Usage: pyspark --packages com.databricks:spark-xml_2.12:0.18.0
 #
+
+import os
+import re
+from typing import List
 
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
+
+
+# This is actually already set in Docker, just reminding you Java is needed
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
+
+# Setup PySpark to use the GraphFrames jar package from maven central
+os.environ["PYSPARK_SUBMIT_ARGS"] = (
+    "--packages com.databricks:spark-xml_2.12:0.18.0 pyspark-shell "
+    "--driver-memory 4g pyspark-shell "
+    "--executor-memory 4g pyspark-shell "
+    "--driver-java-options='-Xmx4g -Xms4g' "
+)
+
+#
+# Section Le Spark...
+#
+
+# Initialize a SparkSession. You can configre SparkSession via: .config("spark.some.config.option", "some-value")
+spark = SparkSession.builder.appName(
+    "Big Graph Builder"
+).getOrCreate()  # Set app name  # Get or create the SparkSession
 
 
 def remove_prefix(df):
@@ -19,10 +44,12 @@ def remove_prefix(df):
     return df.select(s)
 
 
-# Initialize a SparkSession. You can configre SparkSession via: .config("spark.some.config.option", "some-value")
-spark = SparkSession.builder.appName(
-    "Big Graph Builder"
-).getOrCreate()  # Set app name  # Get or create the SparkSession
+@F.udf("array<string>")
+def extract_tags(tags: str) -> List[str]:
+    """Extract the tags from the XML string"""
+    if tags is None:
+        return []
+    return re.findall("<([^>]+)>", tags)
 
 
 # Use Spark-XML to split the XML file into records
@@ -36,8 +63,17 @@ posts_df = (
 # Remove the _ prefix from field names
 posts_df = remove_prefix(posts_df)
 
+# Create a list of tags
+posts_df = (
+    posts_df.withColumn("ParsedTags", extract_tags(posts_df.Tags))
+    .drop("Tags")
+    .withColumnRenamed("ParsedTags", "Tags")
+)
+
 # Write the DataFrame out to Parquet format
-posts_df.write.mode("overwrite").parquet("data/stats.meta.stackexchange.com/Posts.parquet")
+posts_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Posts.parquet"
+)
 
 
 # Use Spark-XML to split the XML file into records
@@ -52,7 +88,9 @@ users_df = (
 users_df = remove_prefix(users_df)
 
 # Write the DataFrame out to Parquet format
-users_df.write.mode("overwrite").parquet("data/stats.meta.stackexchange.com/Users.parquet")
+users_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Users.parquet"
+)
 
 
 # Use Spark-XML to split the XML file into records
@@ -67,4 +105,86 @@ votes_df = (
 votes_df = remove_prefix(votes_df)
 
 # Write the DataFrame out to Parquet format
-votes_df.write.mode("overwrite").parquet("data/stats.meta.stackexchange.com/Votes.parquet")
+votes_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Votes.parquet"
+)
+
+# Use Spark-XML to split the XML file into records
+comments_df = (
+    spark.read.format("xml")
+    .options(rowTag="row")
+    .options(rootTag="comments")
+    .load("data/stats.meta.stackexchange.com/Comments.xml")
+)
+
+# Remove the _ prefix from field names
+comments_df = remove_prefix(comments_df)
+
+# Write the DataFrame out to Parquet format
+comments_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Comments.parquet"
+)
+
+# Use Spark-XML to split the XML file into records
+badges_df = (
+    spark.read.format("xml")
+    .options(rowTag="row")
+    .options(rootTag="badges")
+    .load("data/stats.meta.stackexchange.com/Badges.xml")
+)
+
+# Remove the _ prefix from field names
+badges_df = remove_prefix(badges_df)
+
+# Write the DataFrame out to Parquet format
+badges_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Badges.parquet"
+)
+
+# Use Spark-XML to split the XML file into records
+posthistory_df = (
+    spark.read.format("xml")
+    .options(rowTag="row")
+    .options(rootTag="posthistory")
+    .load("data/stats.meta.stackexchange.com/PostHistory.xml")
+)
+
+# Remove the _ prefix from field names
+posthistory_df = remove_prefix(posthistory_df)
+
+# Write the DataFrame out to Parquet format
+posthistory_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/PostHistory.parquet"
+)
+
+# Use Spark-XML to split the XML file into records
+postlinks_df = (
+    spark.read.format("xml")
+    .options(rowTag="row")
+    .options(rootTag="postlinks")
+    .load("data/stats.meta.stackexchange.com/PostLinks.xml")
+)
+
+# Remove the _ prefix from field names
+postlinks_df = remove_prefix(postlinks_df)
+
+# Write the DataFrame out to Parquet format
+postlinks_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/PostLinks.parquet"
+)
+
+# Use Spark-XML to split the XML file into records
+tags_df = (
+    spark.read.format("xml")
+    .options(rowTag="row")
+    .options(rootTag="tags")
+    .load("data/stats.meta.stackexchange.com/Tags.xml")
+)
+
+# Remove the _ prefix from field names
+tags_df = remove_prefix(tags_df)
+
+# Write the DataFrame out to Parquet format
+tags_df.repartition(1).write.mode("overwrite").parquet(
+    "data/stats.meta.stackexchange.com/Tags.parquet"
+)
